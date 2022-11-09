b7131f78856612780551c1eda11dee15c2a1954f
Hotvizar <vitoks.vs@gmail.com>
2022-11-09 13:56:21 +0300

    Слайдер главной

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
-$( document ).ready(function() {
-    
+$(function () {
+
+    $('#home-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: true,
+		prevArrow: '<button type="button" class="slick-prev"><svg class="icon chevron-left"><use xlink:href="image/icons/symbol/sprite.svg#chevron-left" /></svg></button>',
+		nextArrow: '<button type="button" class="slick-next"><svg class="icon chevron-right"><use xlink:href="image/icons/symbol/sprite.svg#chevron-right" /></svg></button>',
+    });
+
 });
