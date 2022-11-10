4b0f9ee932c1f067b3cfc4aa19d0457633897e45
Hotvizar <vitoks.vs@gmail.com>
2022-11-10 10:59:52 +0300

    афиша готова

--- app/js/main.js
+++ app/js/main.js
@@ -4,10 +4,29 @@ $(function () {
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        speed: 1000,
         dots: true,
         arrows: true,
 		prevArrow: '<button type="button" class="slick-prev"><svg class="icon chevron-left"><use xlink:href="image/icons/symbol/sprite.svg#chevron-left" /></svg></button>',
 		nextArrow: '<button type="button" class="slick-next"><svg class="icon chevron-right"><use xlink:href="image/icons/symbol/sprite.svg#chevron-right" /></svg></button>',
     });
+
+    
+    $('#affiche-slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        speed: 800,
+        dots: true,
+        arrows: true,
+		prevArrow: '<button type="button" class="slick-prev"><svg class="icon chevron-left"><use xlink:href="image/icons/symbol/sprite.svg#chevron-left" /></svg></button>',
+		nextArrow: '<button type="button" class="slick-next"><svg class="icon chevron-right"><use xlink:href="image/icons/symbol/sprite.svg#chevron-right" /></svg></button>',
+    });
+
+
 
 });
