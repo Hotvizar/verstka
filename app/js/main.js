a10d2850c9e5bd91d9447bedc66e8165fff981f7
Hotvizar <vitoks.vs@gmail.com>
2023-05-11 19:50:36 +0300

    header, slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,21 @@
 $(function () {
 
+    $('#home-slider').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: false,
+		arrows: true,
+        prevArrow: '<button type="button" class="slick-prev"><svg class="icon chevron-left"><use xlink:href="image/icons/symbol/sprite.svg#chevron-left" /></svg></button>',
+        nextArrow: '<button type="button" class="slick-next"><svg class="icon chevron-right"><use xlink:href="image/icons/symbol/sprite.svg#chevron-right" /></svg></button>',
+		/* responsive: [
+			{
+				breakpoint: 1300,
+				settings: {
+					arrows: false,
+				}
+			},
+		], */
+	});
 
 });
