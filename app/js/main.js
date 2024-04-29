35f7149e4c8da1fd7f857f604d74c3ab6158cbcd
Hotvizar <vitoks.vs@gmail.com>
2024-04-29 09:15:38 +0300

    header

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,15 @@
 $(function () {
 
+    $("#group-search").on("click", function () {
+        $(this).addClass("active");
+    });
+
+    $(document).on("mouseup", function (e) { // событие клика по веб-документу
+        var div = $("#group-search"); // тут указываем ID элемента
+        if (!div.is(e.target) // если клик был не по нашему блоку
+            && div.has(e.target).length === 0) { // и не по его дочерним элементам
+            div.removeClass("active");
+        }
+    });
 
 });
