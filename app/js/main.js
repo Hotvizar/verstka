43c3560be24dfb474bed06083ebefbebd2b596e3
Hotvizar <vitoks.vs@gmail.com>
2025-03-02 11:31:00 +0300

    home slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,8 @@
 $(function () {
 
+    $(".slider-col").on("mouseenter mouseleave", function() {
+        $(this).toggleClass("big");
+        $(".slider-col").not(this).toggleClass("min");
+    });
 
 });
