5d11dc3c229cb1547c0c1cbe8dfa1da152f96a97
Hotvizar <vitoks.vs@gmail.com>
2026-02-20 18:21:30 +0300

    stomoks start

--- app/js/main.js
+++ app/js/main.js
@@ -2,19 +2,3 @@ $(function () {
 
 
 });
-
-//Добавление класса следующему элементу
-const btn = document.getElementById('capt-plus');
-const next = btn.nextElementSibling;
-
-btn.addEventListener('click', function (event) {
-    event.stopPropagation(); // блокируем всплытие до документа
-    next.classList.toggle('open');
-});
-
-document.addEventListener('click', function (event) {
-    // Если клик не по кнопке и не внутри следующего элемента → убираем класс
-    if (!btn.contains(event.target) && !next.contains(event.target)) {
-        next.classList.remove('open');
-    }
-});
