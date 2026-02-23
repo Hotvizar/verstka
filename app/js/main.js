c5c4696146b9180917a06c5039dbecdc34b187f2
Hotvizar <vitoks.vs@gmail.com>
2026-02-23 12:32:50 +0300

    header

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,16 @@
+const headerSlider = new Swiper('#header-slider', {
+    speed: 1000,
+    spaceBetween: 24,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    /* autoplay: {
+        delay: 5000,
+    }, */
+});
+
 $(function () {
 
 
 });
+
