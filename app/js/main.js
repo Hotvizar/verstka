d58e6884c0b5bde7a299b1e6afc410022312de6c
Hotvizar <vitoks.vs@gmail.com>
2026-02-27 11:10:59 +0300

    home-cases

--- app/js/main.js
+++ app/js/main.js
@@ -9,6 +9,20 @@ const headerSlider = new Swiper('#header-slider', {
     },
 });
 
+const casesSlider = new Swiper('#cases-slider', {
+    slidesPerView: 'auto',
+    freeMode: true,
+    speed: 1000,
+    spaceBetween: 24,
+    /* autoplay: {
+        delay: 5000,
+    }, */
+    navigation: {
+        nextEl: '.btn-slider-next',
+        prevEl: '.btn-slider-prev',
+    },
+});
+
 $(function () {
 
 
